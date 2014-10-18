4fec06b9e0faebe95b970cc06d43a96386c8b2a6
Artyom Shalkhakov <artyom.shalkhakov@gmail.com>
2014-10-18 15:32:38 +0600

    Adding stubbed project support.

---
+++ MonoDevelop.D4Binding/D4Project.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+using MonoDevelop.Core;
+using MonoDevelop.Core.Execution;
+using MonoDevelop.Core.ProgressMonitoring;
+using MonoDevelop.Core.Serialization;
+using MonoDevelop.Ide;
+using MonoDevelop.Projects;
+
+namespace MonoDevelop.D4Binding
+{
+    public class D4Project : Project, ICustomDataItem
+    {
+        #region Properties
+        public override string[] SupportedLanguages
+        {
+            get
+            {
+                return new[]{ "D4", "" };
+            }
+        }
+        public override string[] SupportedPlatforms
+        {
+            get
+            {
+                return new[]{ "AnyCPU" };
+            }
+        }
+        public override string ProjectType
+        {
+            get
+            {
+                return "d4";
+            }
+        }
+
+        public override string ToString ()
+        {
+            return string.Format ("[D4Project: Name={0}]", Name);
+        }
+        #endregion
+
+        #region Init
+        public D4Project (){
+        }
+
+        public D4Project (ProjectCreateInformation info, XmlElement projectOptions)
+        {
+            if (info != null) {
+                Name = info.ProjectName;
+
+                BaseDirectory = info.ProjectBasePath;
+            }
+
+            if (projectOptions != null)
+            {
+            }
+
+            var libs = new List<string> ();
+            if (projectOptions != null) {
+                foreach (XmlNode lib in projectOptions.GetElementsByTagName("Lib"))
+                    if (!string.IsNullOrWhiteSpace (lib.InnerText))
+                        libs.Add (lib.InnerText);
+            }
+
+            // Create a debug configuration
+            var cfg = CreateConfiguration ("Debug") as ProjectConfiguration;
+            DefaultConfiguration = cfg;
+
+            cfg.DebugMode = true;
+            cfg.ExternalConsole = true;
+
+            Configurations.Add (cfg);
+
+            // Create a release configuration
+            cfg = CreateConfiguration ("Release") as ProjectConfiguration;
+
+            cfg.DebugMode = false;
+
+            Configurations.Add (cfg);
+        }
+        #endregion
+
+        #region Build Configurations
+        public override SolutionItemConfiguration CreateConfiguration (string name)
+        {
+            var c = new ProjectConfiguration() { Name=name };
+            if (name.Contains("|"))
+            {
+                c.Platform = name.Substring(name.LastIndexOf('|') + 1);
+                name = name.Substring(0, name.IndexOf('|'));
+            }
+
+            return c;           
+        }
+        #endregion
+
+        #region Building
+        public override bool IsCompileable (string fileName)
+        {
+            return D4LanguageBinding.IsD4File (fileName);
+        }
+
+        /// <summary>
+        /// Returns the absolute file name + path to the link target
+        /// </summary>
+        public override FilePath GetOutputFileName (ConfigurationSelector configuration)
+        {
+            var cfg = GetConfiguration (configuration) as ProjectConfiguration;
+
+            return cfg.OutputDirectory.ToAbsolute(BaseDirectory);
+        }
+        #endregion
+
+        public void Deserialize (ITypeSerializer handler, DataCollection data)
+        {
+            handler.Deserialize (this, data);
+        }
+        public DataCollection Serialize (ITypeSerializer handler)
+        {
+            var ret = handler.Serialize (this);
+            return ret;
+        }
+    }
+}
+
---
+++ MonoDevelop.D4Binding/D4SyntaxMode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+using Mono.TextEditor;
+using Mono.TextEditor.Highlighting;
+
+namespace MonoDevelop.D4Binding
+{
+    public class D4SyntaxMode : SyntaxMode, IDisposable
+    {
+        static SyntaxMode _baseMode;
+
+        public D4SyntaxMode()
+        {
+            var matches = new List<Match>();
+
+            if (_baseMode == null)
+            {
+                var provider = new ResourceStreamProvider(
+                    typeof(D4SyntaxMode).Assembly,
+                    typeof(D4SyntaxMode).Assembly.GetManifestResourceNames().First(s => s.Contains("D4SyntaxMode")));
+                using (Stream s = provider.Open())
+                    _baseMode = SyntaxMode.Read(s);
+            }
+
+            this.rules = new List<Rule>(_baseMode.Rules);
+            this.keywords = new List<Keywords>(_baseMode.Keywords);
+            this.spans = new List<Span>(_baseMode.Spans).ToArray();
+            matches.AddRange(_baseMode.Matches);
+            this.prevMarker = _baseMode.PrevMarker;
+            this.SemanticRules = new List<SemanticRule>(_baseMode.SemanticRules);
+            this.keywordTable = _baseMode.keywordTable;
+            this.keywordTableIgnoreCase = _baseMode.keywordTableIgnoreCase;
+            this.properties = _baseMode.Properties;
+            this.matches = matches.ToArray();
+        }
+
+        public virtual void Dispose() {
+            // nothing as of yet
+        }
+    }
+}
+

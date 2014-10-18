c99af3dca1eb143faee6e3c3b09dc58a293d97bd
Artyom Shalkhakov <artyom.shalkhakov@gmail.com>
2014-10-18 11:41:08 +0600

    Stubbing out the addin. Adding file type definitions and language binding.

---
+++ MonoDevelop.D4Binding/D4LanguageBinding.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using MonoDevelop.Core;
+using MonoDevelop.Projects;
+
+namespace MonoDevelop.D4Binding
+{
+    public class D4LanguageBinding : ILanguageBinding
+    {
+        public string BlockCommentEndTag
+        {
+            get { return "*/"; }
+        }
+
+        public string BlockCommentStartTag
+        {
+            get { return "/*"; }
+        }
+
+        public string Language
+        {
+            get { return "D4"; }
+        }
+
+        public string SingleLineCommentTag
+        {
+            get { return "//"; }
+        }
+
+        public FilePath GetFileName(FilePath fileNameWithoutExtension)
+        {
+            return fileNameWithoutExtension.ChangeExtension(".d4");
+        }
+
+        public static bool IsD4File(string fileName)
+        {
+            return fileName.EndsWith(".d4", System.StringComparison.OrdinalIgnoreCase);
+        }
+
+        public bool IsSourceCodeFile(FilePath fileName)
+        {
+            return IsD4File(fileName);
+        }
+    }
+}
+
---
+++ README.txt
@@ -0,0 +1,4 @@
+D4 IDE
+======
+
+The goal is to create a newer IDE for D4.
